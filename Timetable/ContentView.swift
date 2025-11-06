6eea344d69d978eb67338e43d15aa838f9423741
Thieu Nguyen <thieu.nguyen@niteco.se>
2025-11-06 11:03:15 +0700

    Initial Commit

---
+++ Timetable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timetable
+//
+//  Created by Thieu on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timetable/TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableApp.swift
+//  Timetable
+//
+//  Created by Thieu on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
